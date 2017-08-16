9a0cf07a3b3200237058d2551ed1deed3236c346
rishabh <rishabh.gupta@smartdatainc.net>
2017-08-16 20:16:17 +0530

    build

---
+++ dist/3.chunk.js
@@ -0,0 +1,114 @@
+webpackJsonp([3],{
+
+/***/ "../../../../../src/app/gallery/gallery.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/gallery/gallery.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<section class=\"slider-section-2 slider-section-2-bg\">\n\t\t\t<div class=\"bannar-image\">\n\t\t\t\t<img src=\"assets/images/bannar-img.jpg\" alt=\"bannar\">\n\t\t\t</div><!-- /.bannar-image -->\n\t\t\t<div class=\"container\">\n\t\t\t\t<div class=\"row\">\n\t\t\t\t\t<div class=\"slide\">\t\t\t\t\t\n\t\t\t\t\t\t<div class=\"site-page-header clear-fix\">\n\t\t\t\t\t\t\t<div class=\"site-title\">\n\t\t\t\t\t\t\t\t<h2>Gallery</h2>\n\t\t\t\t\t\t\t</div><!-- /.site-title -->\n\n\t\t\t\t\t\t\t<div class=\"breadcrumb\">\n\t\t\t\t\t\t\t\t<ul>\n\t\t\t\t\t\t\t\t\t<li class=\"breadcrumb-item\"><a href=\"index.html\">Home</a></li>\n\t\t\t\t\t\t\t\t\t<li class=\"breadcrumb-item active\"><a href=\"#\">Gallery</a></li>\n\t\t\t\t\t\t\t\t</ul>\n\t\t\t\t\t\t\t</div><!-- /.breadcrumb -->\n\t\t\t\t\t\t</div><!-- /.site-page-header -->\n\t\t\t\t\t</div><!-- /.slide -->\n\t\t\t\t</div>\n\t\t\t</div><!-- /.container -->\t\n\t\t</section>\n\n\t\t\n\t\t<!--\n\t\t\t\tPhoto Gallery section star \t  =\n\t\t======================================= -->\n\t\t<!-- Portfolio Section -->\n\t\t<section id=\"Gallery-work\" class=\"Gallery-work\">\n\t\t\t<div class=\"section-padding\">\n\t\t\t\t<div class=\"container\">\n\t\t\t\t\t\n\t\t\t\t\t<div id=\"features-container\" class=\"clearfix\">\n\t\t\t\t\t\t<div class=\"featuresFilter\">\n\t\t\t\t\t\t\t<a href=\"#\" data-filter=\"\" class=\"current\">Yoga</a>\n\t\t\t\t\t\t\t<a href=\"#\" data-filter=\".cat-1\">Fitness</a>\n\t\t\t\t\t\t\t<a href=\"#\" data-filter=\".cat-2\">Exercise</a>\n\t\t\t\t\t\t\t<a href=\"#\" data-filter=\".cat-3\">Health</a>\n\t\t\t\t\t\t\t<a href=\"#\" data-filter=\".cat-4\">Bodybuild</a>\n\t\t\t\t\t\t</div> <!-- /.featuresFilter -->\n\n\t\t\t\t\t\t<div class=\"features-item element-from-bottom\">\n\t\t\t\t\t\t\t<figure class=\"item item-w2 cat-1 cat-3\">\n\t\t\t\t\t\t\t\t<img src=\"assets/images/01.jpg\" alt=\"Item 1\" />\n\t\t\t\t\t\t\t\t<div class=\"overlay\"></div>\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t</figure>\n\n\t\t\t\t\t\t\t<figure class=\"item cat-5 cat-2\">\n\t\t\t\t\t\t\t\t<img src=\"assets/images/02.jpg\" alt=\"Item 1\" />\n\t\t\t\t\t\t\t\t<div class=\"overlay\"></div>\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t</figure>\n\n\t\t\t\t\t\t\t<figure class=\"item cat-3 cat-4\">\n\t\t\t\t\t\t\t\t<img src=\"assets/images/03.jpg\" alt=\"Item 1\" />\n\t\t\t\t\t\t\t\t<div class=\"overlay\"></div>\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t</figure>\n\n\t\t\t\t\t\t\t<figure class=\"item cat-5 cat-4\">\n\t\t\t\t\t\t\t\t<img src=\"assets/images/04.jpg\" alt=\"Item 1\" />\n\t\t\t\t\t\t\t\t<div class=\"overlay\"></div>\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t</figure>\n\n\t\t\t\t\t\t\t<figure class=\"item item-w2 cat-2 cat-3\">\n\t\t\t\t\t\t\t\t<img src=\"assets/images/05.jpg\" alt=\"Item 1\" />\n\t\t\t\t\t\t\t\t<div class=\"overlay\"></div>\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t</figure>\n\n\t\t\t\t\t\t\t<figure class=\"item cat-4 cat-1\">\n\t\t\t\t\t\t\t\t<img src=\"assets/images/06.jpg\" alt=\"Item 1\" />\n\t\t\t\t\t\t\t\t<div class=\"overlay\"></div>\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t</figure>\n\n\t\t\t\t\t\t\t<figure class=\"item cat-2 cat-3 cat-5\">\n\t\t\t\t\t\t\t\t<img src=\"assets/images/07.jpg\" alt=\"Item 1\" />\n\t\t\t\t\t\t\t\t<div class=\"overlay\"></div>\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t</figure>\n\n\t\t\t\t\t\t\t<figure class=\"item cat-2 cat-3 cat-5\">\n\t\t\t\t\t\t\t\t<img src=\"assets/images/08.jpg\" alt=\"Item 1\" />\n\t\t\t\t\t\t\t\t<div class=\"overlay\"></div>\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t</figure>\n\n\t\t\t\t\t\t\t<figure class=\"item item-w2 cat-2 cat-3 cat-5\">\n\t\t\t\t\t\t\t\t<img src=\"assets/images/09.jpg\" alt=\"Item 1\" />\n\t\t\t\t\t\t\t\t<div class=\"overlay\"></div>\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t</figure>\n\n\t\t\t\t\t\t</div><!-- /.features-item -->\n\n\t\t\t\t\t\t<a href=\"#\" class=\"load-more text-center\">\n\t\t\t\t\t\t\t<i class=\"fa fa-spinner fa-pulse\"></i>\n\t\t\t\t\t\t\t<span>Load More</span>\n\t\t\t\t\t\t</a><!-- /.load-more -->\n\t\t\t\t\t</div><!-- /#portfolio-container -->\n\t\t\t\t</div>\n\t\t\t</div><!-- /.section-padding -->\n\t\t</section><!-- #portfolio -->"
+
+/***/ }),
+
+/***/ "../../../../../src/app/gallery/gallery.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return GalleryComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var GalleryComponent = (function () {
+    function GalleryComponent() {
+    }
+    GalleryComponent.prototype.ngOnInit = function () {
+    };
+    return GalleryComponent;
+}());
+GalleryComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+        selector: 'app-gallery',
+        template: __webpack_require__("../../../../../src/app/gallery/gallery.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/gallery/gallery.component.css")]
+    }),
+    __metadata("design:paramtypes", [])
+], GalleryComponent);
+
+//# sourceMappingURL=gallery.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/gallery/gallery.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common__ = __webpack_require__("../../../common/@angular/common.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__gallery_component__ = __webpack_require__("../../../../../src/app/gallery/gallery.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_router__ = __webpack_require__("../../../router/@angular/router.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "GalleryModule", function() { return GalleryModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+var routes = [
+    {
+        path: '',
+        component: __WEBPACK_IMPORTED_MODULE_2__gallery_component__["a" /* GalleryComponent */],
+        data: {
+            title: 'gallery',
+        }
+    }
+];
+var GalleryModule = (function () {
+    function GalleryModule() {
+    }
+    return GalleryModule;
+}());
+GalleryModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1__angular_common__["CommonModule"],
+            __WEBPACK_IMPORTED_MODULE_3__angular_router__["a" /* RouterModule */].forChild(routes)
+        ],
+        exports: [__WEBPACK_IMPORTED_MODULE_3__angular_router__["a" /* RouterModule */]],
+        declarations: [__WEBPACK_IMPORTED_MODULE_2__gallery_component__["a" /* GalleryComponent */]]
+    })
+], GalleryModule);
+
+//# sourceMappingURL=gallery.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=3.chunk.js.map
---
+++ dist/8.chunk.js
@@ -0,0 +1,114 @@
+webpackJsonp([8],{
+
+/***/ "../../../../../src/app/aboutus/aboutus.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/aboutus/aboutus.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<section class=\"slider-section-2 slider-section-2-bg\">\n\t\t\t<div class=\"bannar-image\">\n\t\t\t\t<img src=\"assets/images/bannar-img.jpg\" alt=\"bannar\">\n\t\t\t</div><!-- /.bannar-image -->\n\t\t\t<div class=\"container\">\n\t\t\t\t<div class=\"row\">\n\t\t\t\t\t<div class=\"slide\">\t\t\t\t\t\n\t\t\t\t\t\t<div class=\"site-page-header clear-fix\">\n\t\t\t\t\t\t\t<div class=\"site-title\">\n\t\t\t\t\t\t\t\t<h2>About</h2>\n\t\t\t\t\t\t\t</div><!-- /.site-title -->\n\n\t\t\t\t\t\t\t<div class=\"breadcrumb\">\n\t\t\t\t\t\t\t\t<ul>\n\t\t\t\t\t\t\t\t\t<li class=\"breadcrumb-item\"><a [routerLink]=\"['/']\">Home</a></li>\n\t\t\t\t\t\t\t\t\t<li class=\"breadcrumb-item active\"><a [routerLink]=\"['/aboutus']\">About</a></li>\n\t\t\t\t\t\t\t\t</ul>\n\t\t\t\t\t\t\t</div><!-- /.breadcrumb -->\n\t\t\t\t\t\t</div><!-- /.site-page-header -->\n\t\t\t\t\t</div><!-- /.slide -->\n\t\t\t\t</div>\n\t\t\t</div><!-- /.container -->\t\n\t\t</section>\n\n\t\t\n\t\t\t\t<!--\n\t\t\t\tAbout we do section start  \t  =\n\t\t======================================= -->\n\t\t<section id=\"about-we-do\">\n\t\t\t<div class=\"section-padding\">\n\t\t\t\t<div class=\"container\">\n\t\t\t\t\t<div class=\"row\">\n\t\t\t\t\t\t<div class=\"col-md-5\">\n\t\t\t\t\t\t\t<div class=\"about-img\">\n\t\t\t\t\t\t\t\t<img src=\"assets/images/about-we-do.jpg\" alt=\"Images\">\n\t\t\t\t\t\t\t</div><!-- /.about-img -->\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class=\"col-md-7\">\n\t\t\t\t\t\t\t<div class=\"about-content\">\n\t\t\t\t\t\t\t\t<h2>What We Do</h2>\n\t\t\t\t\t\t\t\t<p>Continually brand frictionless manufactured products via just in time mindshare. Quickly optimize exceptional networks before mission-critical web services. cally evolve robust convergence vis-a-vis bricks-and-clicks mindshare. Uniquely strategize enabled scenarios via world-class catalyst for change. Assertively unleash building potentialities whereas distinctive sources.</p>\n\t\t\t\t\t\t\t\t<p>Assertively integrate reliable leadership where maintainable bandwidth. Authoritatively engineer exceptional interfaces before professional methodologies. Rapidiously grow tactical core competencies for interdependent materials.</p>\n\t\t\t\t\t\t\t</div><!-- /.about-content -->\n\t\t\t\t\t\t</div>\n\t\t\t\t\t</div><!-- /.row -->\n\t\t\t\t</div><!-- /.container -->\n\t\t\t</div><!-- /.section-padding -->\n\t\t</section>\n\n\n\n\t\t<!--\n\t\t\t\tPro Trainer section start  \t  =\n\t\t======================================= -->\n\t\t<section id=\"pro-trainer\">\n\t\t\t<div class=\"section-padding\">\n\t\t\t\t<div class=\"container\">\n\t\t\t\t\t<div class=\"row\">\n\t\t\t\t\t\t\n\t\t\t\t\t\t<h2 class=\"section-title\"><span>Professional</span><br> Trainers</h2>\n\t\t\t\t\t\t<div class=\"col-sm-4 col-md-4\">\n\t\t\t\t\t\t\t<div class=\"image-box\">\n\t\t\t\t\t\t\t\t<div class=\"b-img\">\n\t\t\t\t\t\t\t\t\t<a href=\"trainer-portfolio.html\"><img src=\"assets/images/about-trainer-img1.jpg\" alt=\"Trainers Image\"></a>\n\t\t\t\t\t\t\t\t\t<div class=\"overlay\"></div><!-- /.overlay -->\n\t\t\t\t\t\t\t\t\t<div class=\"social-media bounceInDown\">\n\t\t\t\t\t\t\t\t\t\t<a href=\"#\" class=\"social-icon\"><i class=\"fa fa-facebook\"></i></a>\n\t\t\t\t\t\t\t\t\t\t<a href=\"#\" class=\"social-icon\"><i class=\"fa fa-twitter\"></i></a>\n\t\t\t\t\t\t\t\t\t\t<a href=\"#\" class=\"social-icon\"><i class=\"fa fa-pinterest-p\"></i></a>\n\t\t\t\t\t\t\t\t\t\t<a href=\"#\" class=\"social-icon last-border\"><i class=\"fa fa-vimeo-square\"></i></a>\n\t\t\t\t\t\t\t\t\t</div><!-- /.social-media -->\n\t\t\t\t\t\t\t\t</div><!-- /.b-img -->\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t<div class=\"image-des\">\n\t\t\t\t\t\t\t\t\t<h4><a href=\"\">Rafino Yogo</a></h4>\n\t\t\t\t\t\t\t\t\t<p>Fitness Trainer</p>\n\t\t\t\t\t\t\t\t</div><!-- /.image-des -->\n\t\t\t\t\t\t\t</div><!-- /.image-box -->\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class=\"col-sm-4 col-md-4\">\n\t\t\t\t\t\t\t<div class=\"image-box\">\n\t\t\t\t\t\t\t\t<div class=\"b-img\">\n\t\t\t\t\t\t\t\t\t<a href=\"trainer-portfolio.html\"><img src=\"assets/images/about-trainer-img2.jpg\" alt=\"Trainers Image\"></a>\n\t\t\t\t\t\t\t\t\t<div class=\"overlay\"></div><!-- /.overlay -->\n\t\t\t\t\t\t\t\t\t<div class=\"social-media\">\n\t\t\t\t\t\t\t\t\t\t<a href=\"#\" class=\"social-icon\"><i class=\"fa fa-facebook\"></i></a>\n\t\t\t\t\t\t\t\t\t\t<a href=\"#\" class=\"social-icon\"><i class=\"fa fa-twitter\"></i></a>\n\t\t\t\t\t\t\t\t\t\t<a href=\"#\" class=\"social-icon\"><i class=\"fa fa-pinterest-p\"></i></a>\n\t\t\t\t\t\t\t\t\t\t<a href=\"#\" class=\"social-icon last-border\"><i class=\"fa fa-vimeo-square\"></i></a>\n\t\t\t\t\t\t\t\t\t</div><!-- /.social-media -->\n\t\t\t\t\t\t\t\t</div><!-- /.b-img -->\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t<div class=\"image-des\">\n\t\t\t\t\t\t\t\t\t<h4><a href=\"\">Karlose Adriano</a></h4>\n\t\t\t\t\t\t\t\t\t<p>Yoga Trainer</p>\n\t\t\t\t\t\t\t\t</div><!-- /.image-des -->\n\t\t\t\t\t\t\t</div><!-- /.image-box -->\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class=\"col-sm-4 col-md-4\">\n\t\t\t\t\t\t\t<div class=\"image-box\">\n\t\t\t\t\t\t\t\t<div class=\"b-img\">\n\t\t\t\t\t\t\t\t\t<a href=\"trainer-portfolio.html\"><img src=\"assets/images/about-trainer-img3.jpg\" alt=\"Trainers Image\"></a>\n\t\t\t\t\t\t\t\t\t<div class=\"overlay\"></div><!-- /.overlay -->\n\t\t\t\t\t\t\t\t\t<div class=\"social-media\">\n\t\t\t\t\t\t\t\t\t\t<a href=\"#\" class=\"social-icon\"><i class=\"fa fa-facebook\"></i></a>\n\t\t\t\t\t\t\t\t\t\t<a href=\"#\" class=\"social-icon\"><i class=\"fa fa-twitter\"></i></a>\n\t\t\t\t\t\t\t\t\t\t<a href=\"#\" class=\"social-icon\"><i class=\"fa fa-pinterest-p\"></i></a>\n\t\t\t\t\t\t\t\t\t\t<a href=\"#\" class=\"social-icon last-border\"><i class=\"fa fa-vimeo-square\"></i></a>\n\t\t\t\t\t\t\t\t\t</div><!-- /.social-media -->\n\t\t\t\t\t\t\t\t</div><!-- /.b-img -->\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t<div class=\"image-des\">\n\t\t\t\t\t\t\t\t\t<h4><a href=\"\">Zerin Yagrinuqe</a></h4>\n\t\t\t\t\t\t\t\t\t<p>Hard Yoga Teach</p>\n\t\t\t\t\t\t\t\t</div><!-- /.image-des -->\n\t\t\t\t\t\t\t</div><!-- /.image-box -->\n\t\t\t\t\t\t</div>\n\n\n\t\t\t\t\t</div><!-- /.row -->\n\t\t\t\t</div><!-- /.container -->\n\t\t\t</div><!-- /.section-padding -->\n\t\t</section>\n\n\n\n\t\t<!--\n\t\t\t\tqoute section start         =\n\t\t======================================= -->\n\t\t<section id=\"about-qoutation\">\n\t\t\t<div class=\"counter-area\">\n\t\t\t\t\n\t\t\t\t<div class=\"container\">\n\t\t\t\t\t<div class=\"row\">\n\t\t\t\t\t\t<div class=\"col-sm-6 col-md-3\">\n\t\t\t\t\t\t\t<div class=\"counter-item\">\n\t\t\t\t\t\t\t\t<span class=\"number\">15</span>\n\t\t\t\t\t\t\t\t<span class=\"count-name\">Trainers</span>\n\t\t\t\t\t\t\t</div><!-- /.counter-item -->\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class=\"col-sm-6 col-md-3\">\n\t\t\t\t\t\t\t<div class=\"counter-item\">\n\t\t\t\t\t\t\t\t<span class=\"number\">240</span>\n\t\t\t\t\t\t\t\t<span class=\"count-name\">Happy Students</span>\n\t\t\t\t\t\t\t</div><!-- /.counter-item -->\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class=\"col-sm-6 col-md-3\">\n\t\t\t\t\t\t\t<div class=\"counter-item\">\n\t\t\t\t\t\t\t\t<span class=\"number\">24</span>\n\t\t\t\t\t\t\t\t<span class=\"count-name\">Running Classes</span>\n\t\t\t\t\t\t\t</div><!-- /.counter-item -->\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class=\"col-sm-6 col-md-3\">\n\t\t\t\t\t\t\t<div class=\"counter-item\">\n\t\t\t\t\t\t\t\t<span class=\"number\">99</span>\n\t\t\t\t\t\t\t\t<span class=\"count-name\">Exclusive Accessories</span>\n\t\t\t\t\t\t\t</div><!-- /.counter-item -->\n\t\t\t\t\t\t</div>\n\t\t\t\t\t</div><!-- /.row -->\n\t\t\t\t</div><!-- /.container -->\n\t\t\t</div><!-- /.counter-area -->\n\n\t\t\t<!-- Qoute area start -->\n\t\t\t<div id=\"qoute\">\n\t\t\t\n\t\t\t\t<div class=\"container\">\n\t\t\t\t\t<div class=\"flexslider sliding-area\">\n\t\t\t\t\t\t<ul class=\"slides\">\n\t\t\t\t\t\t\t<li data-thumb=\"assets/images/flex-img1.jpg\">\n\t\t\t\t\t\t\t<span class=\"qoute-icon\"><i class=\"fa fa-quote-left\"></i></span>\n\t\t\t\t\t\t\t<p class=\"slideInRight\">Dynamically empower multifunctional e-tailers through professional catalysts for change. Objectively fashion sustainable synergy for world-class results. Credibly predominate one-to-one communities vis-a-vis just in time collaboration and idea sharing. </p>\n\t\t\t\t\t\t\t<h5 class=\"qouter-name\"><a href=\"#\">Megedo Yabo</a></h5>\n\t\t\t\t\t\t\t<span class=\"exertness\">Yoga Expert</span>\n\t\t\t\t\t\t</li>\n\t\t\t\t\t\t<li data-thumb=\"assets/images/flex-img2.jpg\">\n\t\t\t\t\t\t\t<span class=\"qoute-icon\"><i class=\"fa fa-quote-left\"></i></span>\n\t\t\t\t\t\t\t<p>Dynamically empower multifunctional e-tailers through professional catalysts for change. Objectively fashion sustainable synergy for world-class one-to-one communities vis-a-vis just in time collaboration and idea sharing. </p>\n\t\t\t\t\t\t\t<h5 class=\"qouter-name\"><a href=\"#\">Karl Hopper</a></h5>\n\t\t\t\t\t\t\t<span class=\"exertness\">Yoga Expert</span>\n\t\t\t\t\t\t</li>\n\t\t\t\t\t\t<li data-thumb=\"assets/images/flex-img3.jpg\">\n\t\t\t\t\t\t\t<span class=\"qoute-icon\"><i class=\"fa fa-quote-left\"></i></span>\n\t\t\t\t\t\t\t<p>Dynamically empower multifunctional e-tailers through professional catalysts for change. Objectively fashion sustainable synergy for world-class results. Credibly predominate one-to-one Credibly predominate one-to-one communities vis-a-vis just in time collaboration and idea sharing. </p>\n\t\t\t\t\t\t\t<h5 class=\"qouter-name\"><a href=\"#\">Niam Farnando</a></h5>\n\t\t\t\t\t\t\t<span class=\"exertness\">Yoga Expert</span>\n\t\t\t\t\t\t</li>\n\t\t\t\t\t\t</ul>\n\n\t\t\t\t\t</div><!-- /.flexslider -->\n\t\t\t\t</div><!-- /.container -->\n\t\t\t</div>\n\t\t</section>\n\t\t\n\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/aboutus/aboutus.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AboutusComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var AboutusComponent = (function () {
+    function AboutusComponent() {
+    }
+    AboutusComponent.prototype.ngOnInit = function () {
+    };
+    return AboutusComponent;
+}());
+AboutusComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+        selector: 'app-aboutus',
+        template: __webpack_require__("../../../../../src/app/aboutus/aboutus.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/aboutus/aboutus.component.css")]
+    }),
+    __metadata("design:paramtypes", [])
+], AboutusComponent);
+
+//# sourceMappingURL=aboutus.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/aboutus/aboutus.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common__ = __webpack_require__("../../../common/@angular/common.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__aboutus_component__ = __webpack_require__("../../../../../src/app/aboutus/aboutus.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_router__ = __webpack_require__("../../../router/@angular/router.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AboutusModule", function() { return AboutusModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+var routes = [
+    {
+        path: '',
+        component: __WEBPACK_IMPORTED_MODULE_2__aboutus_component__["a" /* AboutusComponent */],
+        data: {
+            title: 'About',
+        }
+    }
+];
+var AboutusModule = (function () {
+    function AboutusModule() {
+    }
+    return AboutusModule;
+}());
+AboutusModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1__angular_common__["CommonModule"],
+            __WEBPACK_IMPORTED_MODULE_3__angular_router__["a" /* RouterModule */].forChild(routes)
+        ],
+        exports: [__WEBPACK_IMPORTED_MODULE_3__angular_router__["a" /* RouterModule */]],
+        declarations: [__WEBPACK_IMPORTED_MODULE_2__aboutus_component__["a" /* AboutusComponent */]]
+    })
+], AboutusModule);
+
+//# sourceMappingURL=aboutus.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=8.chunk.js.map
